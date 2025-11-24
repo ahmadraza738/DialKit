334874d93d6f2d5d134235491c3a536eeb788c23
Revolutic Macboo Air <>
2025-11-24 14:54:32 +0500

    Update to version 1.0.0 - XCFramework ready for release

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let packageVersion = "1.0.2"
+let packageVersion = "1.0.0"
 
 let package = Package(
     name: "DialKit",
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DialKit",
-            url: "https://github.com/ahmadraza738/DialKit/releases/download/1.0.2/DialKit.xcframework.zip",
-            checksum: "ba19260b70d3d4ddff0e44783173d50d9f38e9b469b70c12ea6c9b52276e51eb"
+            url: "https://github.com/ahmadraza738/DialKit/releases/download/1.0.0/DialKit.xcframework.zip",
+            checksum: "8a19798ecde3eef8c4bd4dc805325ffc56df44a0eb6d65e549f35fdf8bc12841"
         )
     ]
 )
