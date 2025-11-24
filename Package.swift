05d842756665fb889a585c8650f82f45c4139137
Ahmad Raza <ahmad.raza@revolutic.net>
2025-11-24 15:40:44 +0500

    Update DialKit binary target URL and checksum

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DialKit",
-            url: "https://github.com/ahmadraza738/DialKit/releases/download/1.0.0/DialKit.xcframework.zip",
-            checksum: "39cb6005c75686cfdc0b52095c86348b6aefb342de2e864b66e0672d8cc6e1f6"
+            url: "https://github.com/ahmadraza738/DialKit/releases/download/0.0.0/DialKit.xcframework.zip",
+            checksum: "2345678"
         )
     ]
 )
